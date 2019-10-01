d633abfad92c87e5c1be84bfcd7e9cef9b2f7007
VeroniqueA <havril777@yahoo.com>
2019-10-01 17:26:47 +0200

    ca ne fonctionne pas mais j'essaye ancore

---
+++ exo_19.rb
@@ -0,0 +1,16 @@
+emailarray = []
+
+for n in (1..50)
+    
+    email = "jean.dupont#{n}@email.fr"
+
+    emailarray = emailarray + [email]
+
+    if n%2 === 0
+       then print emailarray 
+	end
+
+ end
+
+ 
+ 
